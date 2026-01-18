a9e9f45ad5724396af6ae7f7f54e7a013a11e5cd
unknown <chanleo000@gmail.com>
2026-01-18 22:22:29 +0800

    add the get emplyoee function and update employeeController

--- cms-backend/src/main/java/net/javaguides/cms/controller/EmployeeController.java
+++ cms-backend/src/main/java/net/javaguides/cms/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 import net.javaguides.cms.dto.EmployeeDto;
 import net.javaguides.cms.service.EmployeeService;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
---
+++ cms-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,43 @@
+import React,{useEffect,useState} from 'react';
+import { listEmployees } from '../services/EmployeeService';
+
+const ListEmployeeComponent = () => {
+    
+    const [employees, setEmployees] = useState([]);
+    
+    useEffect(() =>{
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        } ).catch(error => {
+             console.error(error);
+        })
+    }, [])
+  return (
+    <div className="container">
+        <h2 className="text-center">List of Employee</h2>
+        <table className="table table-striped table-bordered">
+            <thead>
+            <tr>
+                <th>EMployee Id</th>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>EMployee Email</th>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    employees.map(employee =>
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>)
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
