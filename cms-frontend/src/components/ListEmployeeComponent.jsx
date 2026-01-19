54a5b22d83051f411b73c50402810448593aa38b
unknown <chanleo000@gmail.com>
2026-01-19 16:55:55 +0800

    add the header and footer

--- cms-frontend/src/components/ListEmployeeComponent.jsx
+++ cms-frontend/src/components/ListEmployeeComponent.jsx
@@ -14,28 +14,32 @@ const ListEmployeeComponent = () => {
     }, [])
   return (
     <div className="container">
-        <h2 className="text-center">List of Employee</h2>
-        <table className="table table-striped table-bordered">
-            <thead>
-            <tr>
-                <th>EMployee Id</th>
-                <th>Employee First Name</th>
-                <th>Employee Last Name</th>
-                <th>EMployee Email</th>
-            </tr>
-            </thead>
-            <tbody>
-                {
-                    employees.map(employee =>
-                        <tr key={employee.id}>
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
-                        </tr>)
-                }
-            </tbody>
-        </table>
+        <div className="row">
+            <div className="col-12">
+            <h2 className="text-center mt-4 mb-4">List of Employee</h2>
+            <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>EMployee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>EMployee Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        employees.map(employee =>
+                            <tr key={employee.id}>
+                                <td>{employee.id}</td>
+                                <td>{employee.firstName}</td>
+                                <td>{employee.lastName}</td>
+                                <td>{employee.email}</td>
+                            </tr>)
+                    }
+                </tbody>
+            </table>
+            </div>       
+        </div>
     </div>
   )
 }
